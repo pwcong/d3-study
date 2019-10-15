a8cac0652b8c6037c925d3e52952bfd26ae0918e
彭伟聪 <pengwc@landray.com.cn>
2019-10-15 17:04:58 +0800

    update

--- src/4.hello-chart/index.tsx
+++ src/4.hello-chart/index.tsx
@@ -1,23 +1,114 @@
 import React from 'react';
 import * as d3 from 'd3';
 
-import { buildItems } from '@/utils';
+import { buildData } from '@/utils';
 
 import './style.scss';
 
 export default class App extends React.PureComponent {
-  ref: any;
+  xRef: any;
+  yRef: any;
+
+  margin = {
+    top: 60,
+    bottom: 60,
+    left: 60,
+    right: 60
+  };
+
+  rectHeight = 30;
+  rectWidth = 30;
 
   componentDidMount() {
-    d3.select(this.ref)
-      .selectAll('p')
-      .text((d, i) => `Hello World! ${i}`);
+    this.handleDrawXAxis();
+    this.handleDrawYAxis();
   }
+
+  handleDrawXAxis = () => {
+    const data = buildData(5, {
+      precision: 1,
+      offset: 0,
+      random: {
+        min: 0,
+        max: 5
+      }
+    });
+
+    const scaleLinear = d3
+      .scaleLinear()
+      .domain([0, d3.max(data) || 5])
+      .range([0, 250]);
+
+    const svg = d3.select(this.xRef);
+    const g = svg
+      .append('g')
+      .attr('transform', `translate(${this.margin.top}, ${this.margin.left})`);
+
+    g.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', 20)
+      .attr('y', (d, i) => i * this.rectHeight)
+      .attr('width', d => scaleLinear(d))
+      .attr('height', this.rectHeight - 5)
+      .attr('fill', 'blue');
+
+    const xAxis = d3.axisBottom(scaleLinear).ticks(7);
+    g.append('g')
+      .attr('transform', `translate(20, ${data.length * this.rectHeight})`)
+      .call(xAxis);
+  };
+
+  handleDrawYAxis = () => {
+    const data = buildData(5, {
+      precision: 1,
+      offset: 0,
+      random: {
+        min: 0,
+        max: 5
+      }
+    });
+    const scaleLinear = d3
+      .scaleLinear()
+      .domain([0, d3.max(data) || 5])
+      .range([0, 250]);
+
+    const svg = d3.select(this.yRef);
+    const g = svg
+      .append('g')
+      .attr('transform', `translate(${this.margin.top}, ${this.margin.left})`);
+
+    g.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', (d, i) => i * this.rectWidth + 10)
+      .attr('y', d => 250 - scaleLinear(d))
+      .attr('width', this.rectWidth - 5)
+      .attr('height', d => scaleLinear(d))
+      .attr('fill', 'blue');
+
+    const yAxis = d3
+      .axisLeft(
+        d3
+          .scaleLinear()
+          .domain([0, d3.max(data) || 5])
+          .range([250, 0])
+      )
+      .ticks(7);
+
+    svg
+      .append('g')
+      .attr('transform', `translate(${this.margin.top}, ${this.margin.left})`)
+      .call(yAxis);
+  };
 
   render() {
     return (
       <div className="container">
-        <div ref={ref => (this.ref = ref)}>{buildItems(8)}</div>
+        <svg width="500" height="500" ref={ref => (this.xRef = ref)}></svg>
+        <svg width="500" height="500" ref={ref => (this.yRef = ref)}></svg>
       </div>
     );
   }
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -4,11 +4,26 @@ export const buildData = (
   counts: number,
   options?: {
     offset?: number;
+    precision?: number;
+    random?: {
+      min: number;
+      max: number;
+    };
   }
 ) => {
-  const { offset = 1 } = options || {};
+  const { offset = 1, precision = 0, random = { min: 0, max: 0 } } =
+    options || {};
 
-  return Array.from(Array(counts)).map((_, i) => i + offset);
+  return Array.from(Array(counts)).map((_, i) =>
+    parseFloat(
+      (
+        i +
+        Math.random() * (random.max - random.min) +
+        random.min +
+        offset
+      ).toFixed(precision)
+    )
+  );
 };
 
 export const buildItems = (counts: number) =>
